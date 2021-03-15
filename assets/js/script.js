964462fe0fbf2f6c4be6d9d1adf26de5dfd88803
Adam Natrop <adamnatrop@gmail.com>
2021-03-15 16:23:11 -0500

    added comments throughout code

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,8 @@ var optList = document.getElementById("optionList");
 var startBtn = document.getElementById("start");
 var questionBox = document.getElementById("qBox")
 var initialsForm = document.getElementById("formInitials")
+var initialsInput = document.getElementById("initials");
+var submitBtn = document.getElementById("submitBtn");
 //var btnContainer = document.getElementById("btnContainer")
 
 
@@ -98,34 +100,25 @@ var questionsArray = [
 
 
 
-
-// Functions for program flow
-
-function screenDisplayReset(displayStatus){
-
-
-    startBtn.style.display = "block";
-    gameInfoPTag.style.display = "block";
-    questH1Tag.style.display = "block";
-    
-    startScreenDisplay("none");
-}
-
-
+// starts Quiz 
 startBtn.addEventListener("click", function(event) {
-   
+    // resets question index for second run through
     questionIndex = 0;
-
-    console.log(event)
-
+    // turns off start screen elements
     startBtn.style.display = "none" ;
     gameInfoPTag.style.display = "none";
     questH1Tag.style.display = "none";
-    
+    // goes to questions
     displayQuestion();
 });
 
+// submit score and initials
+submitBtn.addEventListener("click", function(){
+    localStorage.setItem("initials", initialsInput.value);
+    localStorage.setItem("score", highScore);
+})
 
+// END GAME Function
 function endofGame(){
     // Creates div container to store title and buttons
     var gameOverContainer = document.createElement("div");
@@ -146,8 +139,6 @@ function endofGame(){
 
     // shows form to enter initials
     initialsForm.style.display = "block";
-    // logs form to local storage
-    
 
     //creates a div tag, names it playBtn, sets class to button, sets contents to Play Again
     var playBtn = document.createElement("div");
@@ -159,71 +150,62 @@ function endofGame(){
         gameOverContainer.remove();
         displayQuestion();
     })
-
 }
 
-
+// QUESTIONS AND ANSWERS
 function displayQuestion(){
     // hides initials form on second play through
     initialsForm.style.display = "none";
 
+    // creates container for question and answers
     var quizContainer = document.createElement("div");
     quizContainer.setAttribute("id", "qBoxContainer");
     questionBox.appendChild(quizContainer);
 
-    //screenDisplayReset("block");
-
+    // pulls question from question array
     var question = questionsArray[questionIndex];
-    console.log(questionIndex);
-
+    
+    // creates p tag to display question from question object
     var pTag = document.createElement("p");
     pTag.setAttribute("class","gameInfo gameTitle");
     pTag.textContent = question.qDescription;
     quizContainer.appendChild(pTag);
 
+    // runs through each question answer array to create div tag button and display contents
     question.answers.forEach(function(item){
         var div = document.createElement("div");
         div.setAttribute("class", "button");
         div.textContent = item;
-        
         quizContainer.appendChild(div);
-        
+        // adds event lister to each div tag button as its created
         div.addEventListener("click", function(event){
+            // passes through object info to check answers
             checkAnswer(item, question.correctAnswer, quizContainer);
             event.stopPropagation();
         });
     });     
 } 
 
+// CHECK ANSWERS
 function checkAnswer(userAnswer, correctAnswer, quizContainer){
+    // removes question container from DOM
     quizContainer.remove();
-    
+    // checks the answer the user selected against the correct answer stored in the array
     if (userAnswer === correctAnswer){
      highScore++;
     }
-
+    // checks array index to see if it has gone through each question
     if (questionIndex === questionsArray.length - 1){
         questionIndex = 0;
         endofGame();
-        // show high score
-        // end of game stuff
+        
      } else {
+         // increase question index and goes back to display next question
          questionIndex++;
          displayQuestion();
      }
 }
   
-
-function endGameInitials(){
-
-}
-
-
-// INITIAL CALL TO START
-
-//startScreenDisplay("none"); // set to "block" to display
-
-
 
 
 
